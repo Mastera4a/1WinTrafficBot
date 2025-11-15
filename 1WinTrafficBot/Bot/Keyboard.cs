86eca9f674b222e6824b4659dd02c1186945647f
Arkelin <kotarolinn@gmail.com>
2025-11-15 12:58:25 +0200

    Created code for Keyboard.cs for realization multi-language content

--- 1WinTrafficBot/Bot/Keyboard.cs
+++ 1WinTrafficBot/Bot/Keyboard.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace _1WinTrafficBot.Bot
 {
-    internal class Keyboard
+    public static class Keyboard
     {
+        // Главная клавиатура меню
+        public static ReplyKeyboardMarkup MainMenuKeyboard()
+        {
+            return new ReplyKeyboardMarkup(new[]
+            {
+                new KeyboardButton[] { "Заинтересован", "О нас" },
+                new KeyboardButton[] { "Услуги / Цены", "Кейсы" },
+                new KeyboardButton[] { "Сотрудничество", "Связаться с нами" },
+                new KeyboardButton[] { "Сменить язык" }
+            })
+            {
+                ResizeKeyboard = true,
+                OneTimeKeyboard = false
+            };
+        }
+
+        // Клавиатура для кнопки "Заинтересован"
+        public static ReplyKeyboardMarkup InterestedKeyboard()
+        {
+            return new ReplyKeyboardMarkup(new[]
+            {
+                new KeyboardButton[] { "Отправить контакт", "Назад" }
+            })
+            {
+                ResizeKeyboard = true,
+                OneTimeKeyboard = true
+            };
+        }
+
+        // Клавиатура для выбора языка
+        public static ReplyKeyboardMarkup LanguageKeyboard()
+        {
+            return new ReplyKeyboardMarkup(new[]
+            {
+                new KeyboardButton[] { "Русский", "Українська" },
+                new KeyboardButton[] { "English", "عربي" },
+                new KeyboardButton[] { "Назад" }
+            })
+            {
+                ResizeKeyboard = true,
+                OneTimeKeyboard = true
+            };
+        }
     }
 }
