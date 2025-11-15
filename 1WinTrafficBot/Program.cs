d009e2da0097bb075d8c45a28d92c24fe80772ea
Arkelin <kotarolinn@gmail.com>
2025-11-15 07:30:05 +0200

    Added all necessary libraries and created model for storing localized content (by language)

--- 1WinTrafficBot/Program.cs
+++ 1WinTrafficBot/Program.cs
@@ -1,5 +1,27 @@
-﻿namespace _1WinTrafficBot
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace _1WinTrafficBot
 {
+     // Model for storing localized content
+    public class LocalizedContent
+    {
+        public string LanguageCode { get; set; } = "ru";
+        public Dictionary<string, string> Sections { get; set; } = new();
+    }
+
     internal class Program
     {
         static void Main(string[] args)
